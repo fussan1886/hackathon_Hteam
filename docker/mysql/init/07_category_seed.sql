f09a02d75aea558b2e51c533efd8355a101c7553
hareotoko <tigaina2@gmail.com>
2026-08-28 20:51:13 +0900

    カテゴリー選択修正

--- docker/mysql/init/07_category_seed.sql
+++ docker/mysql/init/07_category_seed.sql
@@ -1,4 +1,5 @@
 # テストデータ用
+SET NAMES utf8mb4;
 INSERT INTO categories (category_name)
 VALUES ('コーヒー豆'), ('抽出器具'), ('コーヒーショップ'), ('淹れ方'), ('その他')
 ON DUPLICATE KEY UPDATE category_name = VALUES(category_name);
